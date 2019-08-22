55467df56f85a0c0d40554c1300b47e79ac9c132
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-22 17:17:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,7 +13,18 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  count = 0
+  while coupons[count] do
+    if cart[coupons[count][:item]]
+      coupItem = "#{coupons[count][:item]} W/COUPON"
+      coupPrice = coupons[count][:cost] / coupons[count][:num]
+      cart[coupItem] = {:price => coupPrice, :clearance => true, :count => coupons[count][:num]}
+      count += 1
+    else
+      count += 1
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
